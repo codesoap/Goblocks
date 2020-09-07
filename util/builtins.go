8d551d6e34e84757c032cbd811295c83d8c592df
codesoap <codesoap@mailbox.org>
2020-09-07 23:32:10 +0200

    Apply gofmt

--- util/builtins.go
+++ util/builtins.go
@@ -2,21 +2,22 @@ package util
 
 import (
 	"time"
-	"fmt"
 )
+
 //blockId is automatically allocated
 //send channel is used to update blocks data
 //Gothreads are waked up by messages on rec channels
 //action is a map of whatever was in action json object for corressponding action
 var FunctionMap = map[string]func(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
-	"#Date":Date,
+	"#Date": Date,
 }
+
 //Update time according to "format" property
 func Date(blockId int, send chan Change, rec chan bool, action map[string]interface{}) {
 	run := true
 	for run {
 		send <- Change{blockId, time.Now().Format(action["format"].(string)), true}
 		//Block untill other thread will ping you
-		run = <- rec
+		run = <-rec
 	}
 }
